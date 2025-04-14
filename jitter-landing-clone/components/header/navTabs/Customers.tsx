bf19dca50563ffb0bf0c96ba879d1f344719f0aa
thedunncodes <tobioluwa348@gmail.com>
2025-04-15 00:44:21 +0100

    UPDATE: Content components for products and customers nav drop down

---
+++ jitter-landing-clone/components/header/navTabs/Customers.tsx
@@ -0,0 +1,14 @@
+export default function Customers() {
+    return (
+      <div aria-label='customer-container' className='w-full h-full' >
+          <div aria-label='customer-wrapper' className='lg:w-[80%] xl:w-[75%] m-auto h-full flex flex-wrap border' >
+              <div aria-label="cards-container" className="w-[65%] border" >
+                  s
+              </div>
+              <div aria-label="content-container" className="w-[35%] border" >
+                  s
+              </div>
+          </div>
+      </div>
+    )
+  }
---
+++ jitter-landing-clone/components/header/navTabs/Products.tsx
@@ -0,0 +1,14 @@
+export default function Products() {
+  return (
+    <div aria-label='product-container' className='w-full h-full' >
+        <div aria-label='product-wrapper' className='lg:w-[80%] xl:w-[75%] m-auto h-full flex flex-wrap border' >
+            <div aria-label="cards-container" className="w-[65%] border" >
+                s
+            </div>
+            <div aria-label="content-container" className="w-[35%] border" >
+                s
+            </div>
+        </div>
+    </div>
+  )
+}

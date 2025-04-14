11c0ccabd733fbc222e28ee09a4d1d19f688aeb8
thedunncodes <tobioluwa348@gmail.com>
2025-04-15 00:43:01 +0100

    FIX: changed the layout of the header background to allow expansion

--- jitter-landing-clone/components/header/Header.tsx
+++ jitter-landing-clone/components/header/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import LogoSvg from '../svg/LogoSvg';
 import HamBurger from './HamBurger';
+import Products from './navTabs/Products';
 
 export default function Header() {
     const [ getMenu, setGetMenu ]  = useState<boolean>(false);
@@ -12,11 +13,11 @@ export default function Header() {
 
   return (
     <header className='z-60 h-25 sticky top-0 pt-11 animate-fade-in' >
-        <div aria-label='header-container' className='flex flex-wrap w-full just-fy-center' >
+        <div aria-label='header-container' className='relative flex flex-wrap w-full just-fy-center' >
             <section aria-label='header-content' className={`absolute w-full ${!hideMenuBar? 'animate-pulseY-out' : '-tralate-y-0 animate-pulseY-in'}`} >
                 {/*  */}
-                {/* w-[75%] bg-background border-b border-sec rounded-2xl flex flex-wrap m-auto items-center h-22 shadow-xl */}
-                <div aria-label='content-wrapper' className={`w-[95%] md:w-[70%] lg:w-[80%] xl:w-[75%] bg-background ${(hideMenuBar && (scrollH > 200))? 'border-b border-sec rounded-2xl  shadow-xl' : 'border-transparent'} flex flex-wrap m-auto justify-between lg:justify-normal items-center h-22 transition-all duration-200`} >
+                {/* ${(hideMenuBar && (scrollH > 200))? 'border-b border-sec rounded-2xl  shadow-xl' : 'border-transparent'} */}
+                <div aria-label='content-wrapper' className={`w-[95%] md:w-[70%] lg:w-[80%] xl:w-[75%] bg-backround  flex flex-wrap m-auto justify-between lg:justify-normal items-center h-22 transition-all duration-200`} >
                     <div aria-label='logo-container' className='w-[40%] md:w-[20%] sm:ml-2 lg:ml-0 max-w-[45rem] flex justify-center' >
                         <Link href={'/'} >
                             <LogoSvg /> 
@@ -24,20 +25,28 @@ export default function Header() {
                     </div>
                     <div aria-label='nav-container' className='w-[50%] hidden lg:block' >
                         <nav className='w-full h-full '>
-                            <ul className='group flex w-[90%] h-full gap-x-3 ' >
-                                <li className='grow text-center font-inter group-hover:opacity-40 hover:opacity-100 transition-all duration-300 font-semibold' >
+                            <ul className='group/nav main-menu flex w-[90%] h-full gap-x-3 ' >
+                                <div aria-label='header-bg-wrapper' className='absolute w-full left-0 top-0 -z-10' >
+                                    <div className={`w-[95%] md:w-[70%] lg:w-[80%] xl:w-[75%] m-auto bg-background h-22 menu-expand ${!hideMenuBar? 'animate-pulseY-out' : 'animate-pulseY-in'} ${(hideMenuBar && (scrollH > 200))? 'border-b border-sec rounded-2xl shadow-xl' : 'border-transparent'} transition-all duration-300`} ></div>
+                                </div>
+                                <li className='group/product grow text-center font-inter current-tab group-hover/nav:opacity-40 hover:opacity-100 transition-all duration-300 font-semibold' >
                                     Product
+                                    <div className='absolute bg-background hidden group-hover/product:block w-full h-full mt-15 left-0' >
+                                        <Products />
+                                    </div>
                                 </li>
-                                <li className='grow text-center font-inter group-hover:opacity-40 hover:opacity-100 transition-all duration-300 font-semibold' >
+                                <li className='grow text-center font-inter current-tab group-hover/nav:opacity-40 hover:opacity-100 transition-all duration-300 font-semibold' >
                                     Customers
+                                    <div className='absolute bg-background hidden group-hover/product:block w-full h-full mt-15 left-0' >
+                                    </div>
                                 </li>
                                 <li className='grow' >
-                                    <Link href={'#templates'} className='block text-center font-inter font-semibold group-hover:opacity-40 hover:opacity-100 transition-all duration-300' >
+                                    <Link href={'#templates'} className='block text-center font-inter font-semibold group-hover/nav:opacity-40 hover:opacity-100 transition-all duration-300' >
                                         Templates
                                     </Link>
                                 </li>
                                 <li className='grow' >
-                                    <Link href={'#pricing'} className='block text-center font-inter font-semibold group-hover:opacity-40 hover:opacity-100 transition-all duration-300' >
+                                    <Link href={'#pricing'} className='block text-center font-inter font-semibold group-hover/nav:opacity-40 hover:opacity-100 transition-all duration-300' >
                                         Pricing
                                     </Link>
                                 </li>
@@ -70,6 +79,9 @@ export default function Header() {
                     </div>
                 </div>
             </section>
+            {/* <div className='absolute w-full z-0' >
+                <div className={`w-[95%] md:w-[70%] lg:w-[80%] xl:w-[75%] m-auto bg-red-600 h-22 menu-expand ${!hideMenuBar? 'animate-pulseY-out' : '-tralate-y-0 animate-pulseY-in'} ${(hideMenuBar && (scrollH > 200))? 'border-b border-sec rounded-2xl  shadow-xl' : 'border-transparent'} transition-all duration-200`} ></div>
+            </div> */}
         </div>
     </header>
   )

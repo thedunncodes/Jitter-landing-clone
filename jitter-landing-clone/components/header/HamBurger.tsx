5c1f89f8735db1806578efd635ae5c4610a2e9b8
thedunncodes <tobioluwa348@gmail.com>
2025-04-14 14:33:12 +0100

    FIX: missed animation

--- jitter-landing-clone/components/header/HamBurger.tsx
+++ jitter-landing-clone/components/header/HamBurger.tsx
@@ -37,7 +37,7 @@ export default function HamBurger(props: hamBurgerProps) {
 
   return (
     <div aria-label="hamburger-container" className={`${hide ? 'w-0 -translate-x-14' : 'w-14 -translate-x-0  animate-pulse-shift' } flex items-center justify-center transition-all duration-400 delay-200 ease-out`} >
-        <button aria-label="hamburger" onClick={handleMenu} className={`${hide ? 'scale-0 animate-pulse-exit' : 'w-fit py-3 px-3' } h-full ${ dark ? 'bg-prim' : `${toggle ? 'bg-prim' : 'bg-sec'}` } overflow-hidden transition-all duration-400 delay-200 hover:animate-pulse-grow flex item-center justify-center rounded-full cursor-pointer`} >
+        <button aria-label="hamburger" onClick={handleMenu} className={`${hide ? 'scale-0 animate-pulse-exit' : 'w-fit py-3 px-3 animate-pulse-in' } h-full ${ dark ? 'bg-prim' : `${toggle ? 'bg-prim' : 'bg-sec'}` } overflow-hidden transition-all duration-400 delay-200 hover:animate-pulse-grow flex item-center justify-center rounded-full cursor-pointer`} >
             <div aria-label="hamburger-wrapper" className={`group w-fit flex flex-col justify-between relative ${hide ? '[&>*]:w-0' : '[&>*]:w-7'} [&>*]:py-0 cursor-pointer transition-all delay-300`} >
                 <div aria-label="line-1" className={`${hide ? 'h-0' : 'h-2'} flex justify-end items-center transition-all delay-200`} >
                     <span className={`block w-[90%] ${ dark ? 'bg-white' : `${toggle ? 'bg-white' : 'bg-prim'}` } h-1 rounded-md top-[0.645rem] ${toggle? "w-full absolute rotate-[-135deg]" : ""}  transition-all duration-500`} ></span>

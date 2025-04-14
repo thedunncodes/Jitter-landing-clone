b9fefaa58cc1af3cf8dfed3a540b586017086060
thedunncodes <tobioluwa348@gmail.com>
2025-04-14 10:58:48 +0100

    UPDATE: Header menu animation and responsiveness complete

--- jitter-landing-clone/components/header/HamBurger.tsx
+++ jitter-landing-clone/components/header/HamBurger.tsx
@@ -1,29 +1,55 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 interface hamBurgerProps { 
     toggle?: boolean;
     setToggle?: Dispatch<SetStateAction<boolean>>;
     dark: boolean;
     setDark?: Dispatch<SetStateAction<boolean>>;
+    mobile: boolean;
 }
 
 export default function HamBurger(props: hamBurgerProps) {
-    const { toggle, setToggle, dark } = props;
-    const handleMenu = () => setToggle ? setToggle(!toggle) : true;
+    const { toggle, setToggle, dark, mobile } = props;
+    const [ hide, setHide ] = useState<boolean>(true);
+    const [ lastScrollTop, setLastScrollTop ] = useState<number>(0)
+    const handleMenu = () => {
+        if (setToggle) setToggle(!toggle);
+        if (!mobile) setHide(!hide)
+    };
+
+    useEffect(() => {
+        function handleClickOutside() {
+            if (!mobile) {
+                const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      
+                  if (scrollTop > lastScrollTop){
+                      setHide(false);
+                  } else {
+                      setHide(true);
+                  }
+                  setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop);
+            }
+            
+        }
+    
+        window.addEventListener("scroll", handleClickOutside)
+      })
 
   return (
-    <button aria-label="hamburger" onClick={handleMenu} className={`w-fit h-full ${ dark? 'bg-sec' : 'bg-prim' } flex item-center justify-center py-3 px-3 rounded-full cursor-pointer`} >
-        <div aria-label="hamburger-wrapper" className="group w-fit m-aut0 relative [&>*]:w-7 [&>*]:py-0 cursor-pointer" >
-            <div aria-label="line-1" className=" h-2 flex justify-end items-center mb-[1px]" >
-                <span className={`block w-[90%] ${ dark? 'bg-prim' : 'bg-white' } h-1 rounded-md top-[0.7rem] ${toggle? "w-full absolute rotate-[-135deg]" : ""}  transition-all duration-500`} ></span>
+    <div aria-label="hamburger-container" className={`${hide ? 'w-0 -translate-x-14' : 'w-14 -translate-x-0  animate-pulse-shift' } flex items-center justify-center transition-all duration-400 delay-200 ease-out`} >
+        <button aria-label="hamburger" onClick={handleMenu} className={`${hide ? 'scale-0 animate-pulse-exit' : 'w-fit py-3 px-3' } h-full ${ dark ? 'bg-prim' : `${toggle ? 'bg-prim' : 'bg-sec'}` } overflow-hidden transition-all duration-400 delay-200 hover:animate-pulse-grow flex item-center justify-center rounded-full cursor-pointer`} >
+            <div aria-label="hamburger-wrapper" className={`group w-fit flex flex-col justify-between relative ${hide ? '[&>*]:w-0' : '[&>*]:w-7'} [&>*]:py-0 cursor-pointer transition-all delay-300`} >
+                <div aria-label="line-1" className={`${hide ? 'h-0' : 'h-2'} flex justify-end items-center transition-all delay-200`} >
+                    <span className={`block w-[90%] ${ dark ? 'bg-white' : `${toggle ? 'bg-white' : 'bg-prim'}` } h-1 rounded-md top-[0.645rem] ${toggle? "w-full absolute rotate-[-135deg]" : ""}  transition-all duration-500`} ></span>
+                </div>
+                <div aria-label="line-3" className={`${hide ? 'h-0' : 'h-2'} flex justify-end items-center transition-all delay-200`} >
+                    <span className={`block w-[70%] ${ dark ? 'bg-white' : `${toggle ? 'bg-white' : 'bg-prim'}` } h-1 rounded-md z-10 ${toggle? "w-full -translate-x-4 opacity-0" : ""} transition-all duration-300`} ></span>
+                </div>
+                <div aria-label="line-3" className={`${hide ? 'h-0' : 'h-2'} flex justify-end items-center transition-all delay-200`} >
+                    <span className={`block w-[45%] ${ dark ? 'bg-white' : `${toggle ? 'bg-white' : 'bg-prim'}` } h-1 rounded-md bottom-[0.645rem] ${toggle? "w-full absolute rotate-[135deg]" : ""} transition-all duration-500`} ></span>
+                </div>
             </div>
-            <div aria-label="line-3" className=" h-2 flex justify-end items-center mb-[1px]" >
-                <span className={`block w-[70%] ${ dark? 'bg-prim' : 'bg-white' } h-1 rounded-md z-10 ${toggle? "w-full -translate-x-4 opacity-0" : ""} transition-all duration-300`} ></span>
-            </div>
-            <div aria-label="line-3" className=" h-2 flex justify-end items-center " >
-                <span className={`block w-[45%] ${ dark? 'bg-prim' : 'bg-white' } h-1 rounded-md bottom-[0.7rem] ${toggle? "w-full absolute rotate-[135deg]" : ""} transition-all duration-500`} ></span>
-            </div>
-        </div>
-    </button>
+        </button>
+    </div>
   )
 }
--- jitter-landing-clone/components/header/Header.tsx
+++ jitter-landing-clone/components/header/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     const [ getMenu, setGetMenu ]  = useState<boolean>(false);
 
   return (
-    <header className='relative z-60 h-25 ' >
+    <header className='relative z-60 h-25 sticky top-0' >
         <div aria-label='header-container' className='flex flex-wrap just-fy-center' >
             <section aria-label='header-content' className='absolute border w-full' >
                 <div aria-label='content-wrapper' className='w-[70%] flex flex-wrap m-auto items-center h-22' >
@@ -18,7 +18,7 @@ export default function Header() {
                     <div aria-label='nav-container' className='w-[50%] border' >
                         {/* nav section */}
                     </div>
-                    <div aria-label='login-btn' className='w-[30%] font-inter font-semibold border' >
+                    <div aria-label='login-btn' className='w-[30%] font-inter font-semibold borde' >
                         <button type="button">
                             Log in
                         </button>
@@ -27,28 +27,24 @@ export default function Header() {
             </section>
             <section aria-label='header-menu' className='absolute border-red-600 border w-full' >
                 <div aria-label='menu-container' className='w-[70%] m-auto flex justify-end items-center h-22' >
-                    <div aria-label='menu-wrapper' className='w-fit flex items-center gap-x-5' >
-                        <div aria-label='cta-container' className='font-bold text-lg text-white' >
-                            <Link href={'#try'} className='block w-full h-full bg-prim px-7 py-3 rounded-full' >
+                    <div aria-label='menu-wrapper' className='w-fit flex items-center' >
+                        <div aria-label='cta-container' className='w-fit relative z-20 hidden lg:block h-full flex justify-center items-center font-bold text-lg text-white mr-5' >
+                            <Link href={'#try'} className='block w-fit h-fit bg-prim px-7 py-3 rounded-full hover:animate-pulse-grow' >
                                 Try for free
                             </Link>
                         </div>
-                        <div aria-label='menu-btn-container' >
-                                <HamBurger {...{ dark: false }} /> 
-                        </div>
-                        <div aria-label='mobile-menu-btn-container' >
-                                <HamBurger {...{ toggle: getMenu, setToggle: setGetMenu, dark: true }} />
+                        {
+                            <div aria-label='menu-btn-container' className='relative z-10 hidden lg:block' >
+                                    <HamBurger {...{ dark: true, mobile: false }} /> 
+                            </div>
+                        }
+                        <div aria-label='mobile-menu-btn-container' className='lg:hidden' >
+                                <HamBurger {...{ toggle: getMenu, setToggle: setGetMenu, dark: false, mobile: true }} />
                         </div>
                     </div>
                 </div>
             </section>
         </div>
-        {/*
-            two bg housing:
-                floating desktop bar 
-                mobileMenu
-        */}
-        {/* responsive hamburger and try btn */}
     </header>
   )
 }

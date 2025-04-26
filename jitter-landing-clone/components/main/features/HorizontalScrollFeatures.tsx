0fdc06722c1a45503e3a78eeb1dec2f2b7750161
thedunncodes <tobioluwa348@gmail.com>
2025-04-26 18:30:54 +0100

    FIX: code refractions

--- jitter-landing-clone/components/main/features/HorizontalScrollFeatures.tsx
+++ jitter-landing-clone/components/main/features/HorizontalScrollFeatures.tsx
@@ -1,42 +1,14 @@
 'use client'
-import getObserver from "@/utils/observer"
-import { useEffect } from "react";
+import AnimatedText from "./AnimatedText";
 
 export default function HorizontalScrollFeatures() {
-  const scrollText = [
-    'Jitter', 'makes', 'motion', 'accessible', 'to',
-    'every', 'designer,', 'enabling', 'creative', 'teams',
-    'to', 'collaborate', 'on', 'and', 'deliver', 'engaging', 'animations',
-    'in', 'record', 'time.'
 
-  ];
-
-  useEffect(() => {
-      const scrollPop = () => {
-          const elements = document.querySelectorAll('#scroll-pop');
-          const observer = getObserver('opacity-100');
-          if (elements) elements.forEach(element => {
-              observer.observe(element);
-          });
-      }
-
-      scrollPop();
-      return () => {scrollPop()};
-  }, [])
 
   return (
     <article aria-label="horizontal-features-scroll" >
         <section aria-label="intro" className="w-[70%] m-auto" >
-            <div aria-label="intro-wrapper" className="w-[80%] text-[2.25rem] leading-11 font-bold flex gap-x-1.5 flex-wrap" >
-                { 
-                    scrollText.map((item, index) => {
-                        return (
-                            <div key={index} id="scroll-pop" className="opacity-30" >
-                                {item}
-                            </div>
-                        )
-                    })
-                }
+            <div aria-label="intro-wrapper" className="w-full h-full" >
+                <AnimatedText />
             </div>
         </section>
     </article>

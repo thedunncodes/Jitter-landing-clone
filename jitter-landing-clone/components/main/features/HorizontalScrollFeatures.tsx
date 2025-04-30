2ce6f5ef01a40a798e54fe17ed3f35f954d708f8
thedunncodes <tobioluwa348@gmail.com>
2025-04-30 22:36:43 +0100

    UPDATE: visual update and addition of the parallax zoom component

--- jitter-landing-clone/components/main/features/HorizontalScrollFeatures.tsx
+++ jitter-landing-clone/components/main/features/HorizontalScrollFeatures.tsx
@@ -1,14 +1,18 @@
 'use client'
 import AnimatedText from "./AnimatedText";
+import ParallaxZoom from "./ParallaxZoom";
 
 export default function HorizontalScrollFeatures() {
 
 
   return (
     <article aria-label="horizontal-features-scroll" >
-        <section aria-label="intro" className="w-[70%] m-auto" >
-            <div aria-label="intro-wrapper" className="w-full h-full" >
-                <AnimatedText />
+        <section aria-label="intro" className="w-full m-auto" >
+            <div aria-label="intro-wrapper" className="w-[80%] h-full" >
+              <AnimatedText />
+            </div>
+            <div className="mt-40" >
+              <ParallaxZoom />
             </div>
         </section>
     </article>

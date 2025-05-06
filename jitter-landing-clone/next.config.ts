446f43c8212c9cd5adf4586d1ddcdcd9c6cf5871
thedunncodes <tobioluwa348@gmail.com>
2025-05-06 10:32:30 +0100

    UPDATE: jitter assests route config for images

--- jitter-landing-clone/next.config.ts
+++ jitter-landing-clone/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://assets.jitter.video/horizontal-scroll/**')],
+  },
 };
 
 export default nextConfig;

62336ee7e59de274d10a1a1433b928de51306416
thedunncodes <tobioluwa348@gmail.com>
2025-05-14 13:46:02 +0100

    UPDATE: added a new asset path

--- jitter-landing-clone/next.config.ts
+++ jitter-landing-clone/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://assets.jitter.video/horizontal-scroll/**')],
+    remotePatterns: [new URL('https://assets.jitter.video/horizontal-scroll/**'), new URL('https://jitter.video/aw-cms/**')],
   },
 };
 

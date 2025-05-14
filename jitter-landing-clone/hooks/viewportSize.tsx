82df3e5d33db11418736c76ec120de1f24a25dc9
thedunncodes <tobioluwa348@gmail.com>
2025-05-14 13:54:15 +0100

    UPDATE: tracking of viewport height and file rename

--- jitter-landing-clone/hooks/viewportSize.tsx
+++ jitter-landing-clone/hooks/viewportSize.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 
-export default function GetViewportWidth() {
+export default function GetViewport() {
   const [ vpWidth, setVpWidth ] = useState<number>(0);
+  const [ vpHeight, setVpHeight ] = useState<number>(0);
 
   useEffect(() => {
     function getWidthOnResize() {
-        setVpWidth(window.innerWidth)
+        setVpWidth(window.innerWidth);
+        setVpHeight(window.innerHeight);
     }
 
     setVpWidth(window.innerWidth);
+    setVpHeight(window.innerHeight);
     window.addEventListener('resize', getWidthOnResize);
     return () => window.addEventListener('resize', getWidthOnResize);
   }, [])
 
-  return [vpWidth]
+  return [vpWidth, vpHeight]
   
 }

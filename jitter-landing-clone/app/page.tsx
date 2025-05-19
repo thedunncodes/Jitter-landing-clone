7d42fe3c2346c64edf82be9f5ae64ccdfc0211f1
thedunncodes <tobioluwa348@gmail.com>
2025-05-19 14:12:31 +0100

    UPDATE: Implementation of horizontalScrollFeatures component and Lenis

--- jitter-landing-clone/app/page.tsx
+++ jitter-landing-clone/app/page.tsx
@@ -1,11 +1,28 @@
+'use client'
 import Header from "@/components/header/Header";
+import HorizontalScrollFeatures from "@/components/main/features/HorizontalScrollFeatures";
+import CtaHover from "@/components/main/hovers/CtaHover";
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect( () => {
+    const lenis = new Lenis()
+   
+    function raf(time: number) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  },[])
+
   return (
     <>
       <Header />
-      <main className="mt-22" >
-          Hello Jitter
+      <main className="mt-22 w-full" >
+        <div className="h-[25rem] w-full bg-sky-600s" ></div>
+          <HorizontalScrollFeatures />
       </main>
     </>
   );
